a962081cd40c248b51acef6fdc9c407df06d010c
mirrorgo <cloudmirrorgo@163.com>
2022-03-05 11:46:55 +0800

    axios

--- src_crx/src_popup/App.tsx
+++ src_crx/src_popup/App.tsx
@@ -1,11 +1,25 @@
-import { argv } from "config/env";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-  console.log(argv);
+  //https://qcnxpt.api.cloudendpoint.cn/helloworld
+  const [testApi, setTestApi] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://qcnxpt.api.cloudendpoint.cn/helloworld")
+      .then((re) => setTestApi(re.data.test));
+
+    axios
+      .post("https://qcnxpt.api.cloudendpoint.cn/helloworld", {
+        hello: "post测试啊",
+      })
+      .then((re) => console.log(re, "post本地"));
+  }, []);
+
   return (
     <div>
       popup
-      <h1>{argv}</h1>
-      {import.meta.env.MODE === "development" ? "a" : "b"}
+      <h1>popup</h1>
+      <div>{testApi}</div>
     </div>
   );
 }
