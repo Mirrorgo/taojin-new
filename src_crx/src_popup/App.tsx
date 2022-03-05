7424d632ddbe893e4332af3eb014c8a9fde9231e
mirrorgo <cloudmirrorgo@163.com>
2022-03-05 13:24:44 +0800

    ts env

--- src_crx/src_popup/App.tsx
+++ src_crx/src_popup/App.tsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { argv } from "config/env";
 function App() {
   //https://qcnxpt.api.cloudendpoint.cn/helloworld
   const [testApi, setTestApi] = useState("");
+  // useEffect(() => {
+  //   axios
+  //     .get("https://qcnxpt.api.cloudendpoint.cn/helloworld")
+  //     .then((re) => setTestApi(re.data.test));
+
+  //   axios
+  //     .post("https://qcnxpt.api.cloudendpoint.cn/helloworld", {
+  //       hello: "post测试啊",
+  //     })
+  //     .then((re) => console.log(re, "post本地"));
+  // }, []);
   useEffect(() => {
-    axios
-      .get("https://qcnxpt.api.cloudendpoint.cn/helloworld")
-      .then((re) => setTestApi(re.data.test));
-
-    axios
-      .post("https://qcnxpt.api.cloudendpoint.cn/helloworld", {
-        hello: "post测试啊",
-      })
-      .then((re) => console.log(re, "post本地"));
+    console.log(argv);
   }, []);
-
   return (
     <div>
       popup
       <h1>popup</h1>
       <div>{testApi}</div>
+      {/* <h2>{argv}</h2> */}
     </div>
   );
 }
--- src_global/config/env.ts
+++ src_global/config/env.ts
@@ -12,16 +12,28 @@
 //     apiBaseUrl: "//www.xxx.com/v1", // 正式环境接口地址
 //   },
 // };
-const mode = import.meta.env.MODE;
-export let argv: string;
-switch (mode) {
-  case "development":
-    argv = "dev";
-    break;
-  case "release":
-    argv = "res";
-    break;
-  default:
-    argv = "default";
-    break;
+export { argv };
+type ObjOrFalse = object | false;
+const argv: ObjOrFalse = iniArgvByMode(import.meta.env.MODE);
+function iniArgvByMode(mode: string): ObjOrFalse {
+  switch (mode) {
+    case "development":
+      return {
+        cdn: "./",
+        apiBaseUrl: "/api", // 开发环境接口请求，后用于 proxy 代理配置
+      };
+    case "beta":
+      return {
+        cdn: "//s.xxx.com/vite-react-app/beta", // 测试环境 cdn 路径
+        apiBaseUrl: "//www.beta.xxx.com/v1", // 测试环境接口地址
+      };
+    case "release":
+      return {
+        cdn: "//s.xxx.com/vite-react-app/release", // 正式环境 cdn 路径
+        apiBaseUrl: "//www.xxx.com/v1", // 正式环境接口地址
+      };
+    default:
+      return false;
+  }
 }
+//https://qcnxpt.api.cloudendpoint.cn/helloworld
